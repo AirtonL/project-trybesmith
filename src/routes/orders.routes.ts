1cfcc78be51e0f0f573bdf9590679105be68f837
Airtonl <junynnhoh@gmail.com>
2022-05-15 11:27:05 -0300

    feat:  create orders register route

--- src/routes/orders.routes.ts
+++ src/routes/orders.routes.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 
 import OrdersController from '../controllers/orders/orders.controller';
+import verify from '../middlewares/auth';
+import validOrders from '../middlewares/orders.create.middlewares';
 
 const ordersController = new OrdersController();
 
 const routerOrders = Router();
 
 routerOrders.get('/orders', ordersController.getAll);
+
+routerOrders.post('/orders', verify, validOrders, ordersController.create);
 
 export default routerOrders;

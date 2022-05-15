4c911c5520a2d41f739b3edf7d774d53dc8fe939
Airtonl <junynnhoh@gmail.com>
2022-05-15 15:20:30 -0300

    style: simplifies message comparison

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -1,15 +1,14 @@
 import type { ErrorRequestHandler } from 'express';
 
 const error: ErrorRequestHandler = (
-  err,
+  { isJoi, message },
   _req,
   res,
   _next,
 ) => {
-  if (err.details[0].message === `"${err.details[0].path[0]}" is required`) {
-    return res.status(400).json({ message: err.message });
-  }
-  if (err.isJoi) return res.status(422).json({ message: err.message });
+  if (message.includes('required')) return res.status(400).json({ message });
+
+  if (isJoi) return res.status(422).json({ message });
 
   return res.status(500).json({ message: 'Server error' });
 };

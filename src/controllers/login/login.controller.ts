8ae8308bbb511b0d69437a7b90a771c26a723913
Airtonl <junynnhoh@gmail.com>
2022-05-15 11:25:23 -0300

    feat: create login controller

---
+++ src/controllers/login/login.controller.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction } from 'express';
+import LoginService from '../../services/login/login.service';
+
+class LoginController {
+  constructor(public loginService = new LoginService()) {}
+
+  public auth = async (req: Request, res: Response, next: NextFunction) => {
+    const { username, password } = req.body;
+    try {
+      const { message, token } = await this.loginService.auth({ username, password });
+
+      if (message) return res.status(401).json({ message });
+
+      return res.status(200).json({ token });
+    } catch (error) {
+      next(error);
+    }
+  };
+}
+
+export default LoginController;

4dfbc17c4d11e8d9b7b90280051ec94dd3619e15
Airtonl <junynnhoh@gmail.com>
2022-05-15 11:29:19 -0300

    feat: create auth middleware

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,30 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+interface Ipayload {
+  data: {
+    id: number;
+  };
+}
+
+const verify = (req: Request, res: Response, next: NextFunction) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({
+      message: 'Token not found',
+    });
+  }
+
+  try {
+    const { data } = jwt.verify(token, 'ARE SECRET') as Ipayload;
+    req.userId = data.id;
+    next();
+  } catch (err: any) {
+    return res.status(401).json({
+      message: 'Invalid token',
+    });
+  }
+};
+
+export default verify;

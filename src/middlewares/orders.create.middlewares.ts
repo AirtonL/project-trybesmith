0fcd8b5d4b1ac7f0249c4e5a8b029428f086381f
Airtonl <junynnhoh@gmail.com>
2022-05-15 11:29:57 -0300

    feat: create orders middleware

---
+++ src/middlewares/orders.create.middlewares.ts
@@ -0,0 +1,18 @@
+import { Request, Response, NextFunction } from 'express';
+import Joi from 'joi';
+
+const ordersSchema = Joi.object().keys({
+  productsIds: Joi.array().required().min(1)
+    .messages({
+      'array.min': '"productsIds" must include only numbers',
+    }),
+});
+
+const validOrders = (req: Request, _res: Response, next: NextFunction) => {
+  const { error } = ordersSchema.validate(req.body);
+  if (error) throw error;
+
+  next();
+};
+
+export default validOrders;
